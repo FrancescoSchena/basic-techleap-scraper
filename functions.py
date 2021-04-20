316d975c0ff59eba3349661837b9698e3994a4e0
Francesco Schena <francesco@schena.eu>
2021-04-20 22:02:11 +0200

    Multiprocessing - contact page - toggles

--- functions.py
+++ functions.py
@@ -6,18 +6,29 @@ import re
 if __name__ == "__main__":
 	print("Not supposed to be run this way")
 
+# Printing functions
+def line(number=1):
+	print("\n"*number)
+	return
+
 # Generating list of company links
 def get_complink(link, complinks):
-	url = "https://finder.techleap.nl" + link
-	page = urlopen(url)
+	fancy_link= link.replace("_", "")
+	url = "https://finder.techleap.nl" + fancy_link
+	keepgoing = False
+	try:
+		req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"}) 
+		page = urlopen(req)
+		keepgoing = True
+	except Exception as e:
+		print("error:\t" + str(e))
 	soup = BeautifulSoup(page, 'html.parser')
 	
-	fancy_link= link.replace("_", "")
-	save = True
 	for href in soup.find_all('a'):
 		templink = href.get('href')
-		if fancy_link.split("/")[2] in templink and "http" in templink:
-			for domain in ["facebook", "google", "linkedin", "twitter", "apple"]:
+		if fancy_link.split("/")[-1] in templink and "http" in templink:
+			save = True
+			for domain in ["facebook", "google", "linkedin", "twitter", "apple", "dealroom"]:
 				if domain in templink:
 					save = False
 			if save:
@@ -26,21 +37,48 @@ def get_complink(link, complinks):
 
 # Searching each link for emails
 def get_email(link, compmails):
-	print("*"*10)
-	print(link)
 	try:
 		req = urllib.request.Request(link, headers={'User-Agent' : "Magic Browser"}) 
 		con = urlopen(req)
+		keepgoing = True
 	except Exception as e:
-		pass
-	s = con.read().decode("utf-8")
-	emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", s)
-	newemails = list(set(emails))
-	for mail in newemails:
-		if mail.split(".")[-1] == link.split(".")[-1] and "%20" not in mail:
+		print(f" * * * Error opening {link}")
+		print(" * * * " + str(e))
+		keepgoing = False
+	if keepgoing:
+		mailexists = False
+		s = con.read().decode("utf-8")
+		emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", s)
+		newemails = list(set(emails))
+		for mail in newemails:
+			if mail.split(".")[-1] == link.split(".")[-1]:  # Checks the .com/whateves
+				mailexists = True
+				try:
+					compmails[link].append(mail)
+				except KeyError:
+					compmails[link] = [mail]
+		if not mailexists:
 			try:
-				compmails[link].append(mail)
-			except KeyError:
-				compmails[link] = [mail]
-			print(link, "\t", mail)
-	print("*"*35)
+				if link[-1] == "/":
+					spacer = ""
+				else:
+					spacer = "/"
+				req = urllib.request.Request(link + spacer + "contact", headers={'User-Agent' : "Magic Browser"}) 
+				con = urlopen(req)
+				keepgoing = True
+			except Exception as e:
+				print(f" * * * Error opening {link + spacer + 'contact'}")
+				print(" * * * " + str(e))
+				keepgoing = False
+			if keepgoing:
+				s = con.read().decode("utf-8")
+				emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", s)
+				newemails = list(set(emails))
+				for mail in newemails:
+					if mail.split(".")[-1] == link.split(".")[-1]:  # Checks the .com/whateves
+						mailexists = True
+						try:
+							compmails[link].append(mail)
+						except KeyError:
+							compmails[link] = [mail]
+		
--- run.py
+++ run.py
@@ -4,36 +4,50 @@ import multiprocessing
 import functions
 import re
 
+externalfile = False
+only_complinks = True
+
 
 if __name__ == "__main__":
-	# Looking for the files in the /data folder
-	mypath = os.path.abspath(os.getcwd()) + "/data"
-	ext = "txt"
-	files = []
-	for (dirpath, dirnames, filenames) in walk(mypath):
-		for file in filenames:
-			if file.split(".")[1] == ext:  # Only files with the right extension
-				files.append(file)
-		break
-	if not files:  # if files is empty
-		raise Exception("Warning there are no files, check the path!")
-		quit()  # no need to proceed
+	if externalfile:
+		# Looking for the files in the /data folder
+		mypath = os.path.abspath(os.getcwd()) + "/data"
+		ext = "txt"
+		files = []
+		for (dirpath, dirnames, filenames) in walk(mypath):
+			for file in filenames:
+				if file.split(".")[1] == ext:  # Only files with the right extension
+					files.append(file)
+			break
+		if not files:  # if files is empty
+			raise Exception("Warning there are no files, check the path!")
+			quit()  # no need to proceed
 
-	s = "" if len(files) == 1 else "s"
-	print(f"Analyzing {len(files)} file{s}:")
-	[print("\t" + file) for file in files]
+		s = "" if len(files) == 1 else "s"
+		print(f"Analyzing {len(files)} file{s}:")
+		[print("\t" + file) for file in files]
 
+		functions.line(2)
 
-	# Generating list of links from the txt
-	links = []
-	for file in files:
-		file = open(f"Data/{file}", "r").read()
-		for i in [m.start() for m in re.finditer('href', file)]:
-			closingcommas = file[i : i+50].find(">")
-			link = file[i+6 : i+closingcommas-1]
-			links.append(link)
+		# Generating list of links from the txt
+		links = []
+		for file in files:
+			file = open(f"Data/{file}", "r").read()
+			for i in [m.start() for m in re.finditer('href', file)]:
+				closingcommas = file[i : i+50].find(">")
+				link = file[i+6 : i+closingcommas-1]
+				if link:
+					links.append(link)
+
+	else:  # Fallback
+		links = ['/companies/scope_biosciences', '/companies/luuno', '/companies/deeptrial', '/companies/arize', '/companies/amberscript', '/companies/be_sure_healthcare_b_v_', '/companies/join', '/companies/vitestro', '/companies/recheck', '/companies/frame_therapeutics', '/companies/the_close_app', '/companies/we4sea', '/companies/neurolytics', '/companies/hellomaas', '/companies/go_build_it_bouw7_']
 
 	print("Starting processes")
+
+	# Printing links
+	print(f"There are {len(links)} links")
+	functions.line()
+	print("Finding links...")
 
 	# Generating list of company links
 	manager = multiprocessing.Manager()
@@ -47,7 +61,24 @@ if __name__ == "__main__":
 	for proc in jobs:
 		proc.join()
 
-	# print(complinks)
+
+	# Printing links
+	functions.line(1)
+	print(f"There are {len(links)} links")
+	functions.line(1)
+
+	# Printing complinks
+	print(f"There are {len(complinks)} comp_links")
+
+	if externalfile:
+		with open('complinks.txt', 'w') as f:  # Saving the complinks for later ;)
+			for i in complinks:
+				f.write(i+"\n")
+
+	if only_complinks:
+		quit()
+	functions.line(1)
+	print("Finding mails...")
 
 	# Searching each link for emails
 	manager = multiprocessing.Manager()
@@ -61,9 +92,12 @@ if __name__ == "__main__":
 	for proc in jobs:
 		proc.join()
 
-'''	print(compmails)
+	functions.line(2)
 
 	# Printing mails
+	print(f"There are {len(compmails)} mails")
+	
+
 	for i in compmails:
 		print(i)
-		[print("\t" + x) for x in compmails[i]]'''
+		[print("\t" + x) for x in compmails[i]]
